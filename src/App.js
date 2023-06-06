6cc312f3898b3326b9b28de159ef574dc384b642
intern <intern@interns-MacBook-Pro.local>
2023-06-06 17:04:20 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>github actions testing</h1>
+          <h1>github actions testing with manual trigger</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
